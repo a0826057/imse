926943a16018266301a52fbf7999ea44ae8b9559
Ines Bacic <a1349198@unet.univie.ac.at>
2017-12-23 20:02:27 +0100

    Accesory- and ModelManagement Classes Function Updates

--- src/management/AccessoryManagement.java
+++ src/management/AccessoryManagement.java
@@ -1,5 +1,62 @@
 package management;
 
-public class AccessoryManagement {
+import java.util.List;
+import imse.Accessory;
 
+public class AccessoryManagement {
+	List<Accessory> accessory;
+	
+	public AccessoryManagement(){
+		//accessory = new List<Accessory>();
+	}
+
+	public List<Accessory> getAccessory() {
+		return accessory;
+	}
+
+	public void setAccessory(List<Accessory> accessory) {
+		this.accessory = accessory;
+	}
+	
+	public Accessory getAccessoryById(int accessory_id){
+		for(Accessory acc : accessory){
+			if(acc.getAccessory_ID() == accessory_id){
+				return acc;
+			}
+		}
+		return null;
+	}
+	
+	public void addAccessory(String name, String description){
+		//CHECK IF THE SAME ACCESSORY IS THERE BY NAME
+		//Accessory acc = new Accessory(name,description);
+		//accessory
+	}
+	
+	public void changeAccessory(String name, String description){
+		
+	}
+	
+	public void deleteAccessory(int accessory_id){
+		for(Accessory acc : accessory){
+			if(acc.getAccessory_ID() == accessory_id){
+				accessory.remove(acc);
+				break;
+			}
+		}
+	}
+	
+	
+	public int getAccessoryCount(){
+		return accessory.size();
+	}
+	
+	public Accessory getAccessoryByName(String name){
+		for(Accessory acc : accessory){
+			if(acc.getName() == name){
+				return acc;
+			}
+		}
+		return null;
+	}
 }
--- src/management/ModelManagement.java
+++ src/management/ModelManagement.java
@@ -1,5 +1,52 @@
 package management;
 
-public class ModelManagement {
+import java.util.List;
 
+import imse.Accessory;
+import imse.Manufacturer;
+import imse.Model;
+
+public class ModelManagement {
+	List<Model> models;
+
+	public ModelManagement(){
+		//models = 
+	}
+	
+	public List<Model> getModels() {
+		return models;
+	}
+
+	public void setModels(List<Model> models) {
+		this.models = models;
+	}
+	
+	public Model getModelById(int model_id){
+		for(Model mod : models){
+			if(mod.getModel_ID() == model_id){
+				return mod;
+			}
+		}
+		return null;
+	}
+	
+	public void addModel(Manufacturer manufacturer, double price){
+		
+	}
+	
+	public void changeModel(Manufacturer manufacturer, double price){
+		
+	}
+	
+	public void deleteModel(int model_id){
+		
+	}
+	
+	public int getModelCount(){
+		return models.size();
+	}
+	
+	public Model getModelByManufacturerName(String name){
+		return null;
+	}
 }
