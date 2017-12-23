19fedce8ec199e2d5ccc4a5716160f6abfdabc1e
Ines Bacic <a1349198@unet.univie.ac.at>
2017-12-23 22:07:22 +0100

    RentalManagement functions update

--- src/management/RentalManagement.java
+++ src/management/RentalManagement.java
@@ -1,5 +1,47 @@
 package management;
 
-public class RentalManagement {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import imse.Rental;
+import imse.Vehicle;
 
+public class RentalManagement {
+	List<Rental> rentals;
+
+	public RentalManagement(){
+		this.rentals = new ArrayList<Rental>();
+	}
+	
+	public RentalManagement(List<Rental> ls){
+		this.rentals = ls;
+	}
+	
+	public List<Rental> getRentalList() {
+		return rentals;
+	}
+
+	public void setRentals(List<Rental> rentals) {
+		this.rentals = rentals;
+	}
+	
+	public Rental getRentalById(int rental_id){
+		return null;
+	}
+	
+	public void deleteRental(int rental_id){
+		
+	}
+	
+	public int getRentalCount(){
+		return rentals.size();
+	}
+	
+	public Rental getRentalByVehicle(Vehicle vehicle){
+		return null;
+	}
+	
+	public Rental getRentalByDatePeriod(Date date_from, Date date_to){
+		return null;
+	}
 }
