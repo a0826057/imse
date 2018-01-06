85eb7a7cbb38e330b5af00e13f4f4dca53746f08
Betül Sahin <a11709872@unet.univie.ac.at>
2018-01-06 23:12:00 +0300

    update

--- src/dao/VehicleDAOI.java
+++ src/dao/VehicleDAOI.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -71,7 +72,10 @@ public class VehicleDAOI implements VehicleDAO{
 			
 			Statement statement = con.createStatement();
 			statement.setQueryTimeout(60);
-			ResultSet result = statement.executeQuery("SELECT * FROM vehicle WHERE vehicle_ID = " + vehicle_ID + ";");
+			String raw_query = "SELECT * FROM vehicle WHERE vehicle_ID = ?";
+			PreparedStatement prepared = con.prepareStatement(raw_query);
+			prepared.setInt(1, vehicle_ID);
+			ResultSet result = prepared.executeQuery();
 			
 			while(result.next()){
 				Color c = col.getColorById( result.getInt("color_ID"));
@@ -145,24 +149,25 @@ public class VehicleDAOI implements VehicleDAO{
 			
 			Statement statement = con.createStatement();
 			statement.setQueryTimeout(60);
-			statement.executeUpdate("INSERT INTO vehicle(lisence_plate_number,color_ID,model_ID,manufacturer_ID,accessory_ID,mileage,manufacturer_year,active) VALUES(" +
-									plate + "," +
-									color.getColor_ID() + "," +
-									model.getModel_ID() + "," +
-									manufacturer.getManufacturer_ID() + "," +
-									accessory.getAccessory_ID() + "," +
-									mileage + "," +
-									year + "," +
-									active +
-									");"
-									);
+			String raw_query = "INSERT INTO vehicle(lisence_plate_number,color_ID,model_ID,manufacturer_ID,accessory_ID,mileage,manufacturer_year,active) VALUES(?,?,?,?,?,?,?,?)";
+			PreparedStatement prepared = con.prepareStatement(raw_query);
+			prepared.setString(1, plate);
+			prepared.setInt(2, color.getColor_ID());
+			prepared.setInt(3,model.getModel_ID());
+			prepared.setInt(4, manufacturer.getManufacturer_ID());
+			prepared.setInt(5, accessory.getAccessory_ID());
+			prepared.setInt(6, mileage);
+			prepared.setInt(7, year);
+			prepared.setBoolean(8, active);
+			prepared.executeUpdate();
 			VehicleDAOI veh = new VehicleDAOI();
 			List<Vehicle> lsv = veh.getVehicleList();
-			statement.executeUpdate("INSERT INTO car(car_ID,doors,passenger_limit) VALUES(" +
-															lsv.get(lsv.size()-1).getVehicle_ID() + "," +
-															doors + "," +
-															pass_limit +
-															");");
+			String raw_query2 = "INSERT INTO car(car_ID,doors,passenger_limit) VALUES(?,?,?)";
+			PreparedStatement prepared2 = con.prepareStatement(raw_query2);	
+			prepared2.setInt(1, lsv.get(lsv.size()-1).getVehicle_ID());
+			prepared2.setInt(2, doors);
+			prepared2.setInt(3, pass_limit);
+			prepared2.executeUpdate();
 		}catch(Exception e){
 			System.err.println(e);
 		}finally {
@@ -183,25 +188,26 @@ public class VehicleDAOI implements VehicleDAO{
 			
 			Statement statement = con.createStatement();
 			statement.setQueryTimeout(60);
-			statement.executeUpdate("INSERT INTO vehicle(lisence_plate_number,color_ID,model_ID,manufacturer_ID,accessory_ID,mileage,manufacturer_year,active) VALUES(" +
-									plate + "," +
-									color.getColor_ID() + "," +
-									model.getModel_ID() + "," +
-									manufacturer.getManufacturer_ID() + "," +
-									accessory.getAccessory_ID() + "," +
-									mileage + "," +
-									year + "," +
-									active +
-									");"
-									);
+			String raw_query = "INSERT INTO vehicle(lisence_plate_number,color_ID,model_ID,manufacturer_ID,accessory_ID,mileage,manufacturer_year,active) VALUES(?,?,?,?,?,?,?,?)";
+			PreparedStatement prepared = con.prepareStatement(raw_query);
+			prepared.setString(1, plate);
+			prepared.setInt(2, color.getColor_ID());
+			prepared.setInt(3,model.getModel_ID());
+			prepared.setInt(4, manufacturer.getManufacturer_ID());
+			prepared.setInt(5, accessory.getAccessory_ID());
+			prepared.setInt(6, mileage);
+			prepared.setInt(7, year);
+			prepared.setBoolean(8, active);
+			prepared.executeUpdate();					
 			VehicleDAOI veh = new VehicleDAOI();
 			List<Vehicle> lsv = veh.getVehicleList();
-			statement.executeUpdate("INSERT INTO truck(truck_ID, length, height, loading_limit) VALUES(" +
-															lsv.get(lsv.size()-1).getVehicle_ID() + "," +
-															length + "," +
-															height +","+
-															load_limit+
-															");");
+			String raw_query2 = "INSERT INTO truck(truck_ID, length, height, loading_limit) VALUES(?,?,?,?)";
+			PreparedStatement prepared2 = con.prepareStatement(raw_query2);
+			prepared2.setInt(1, lsv.get(lsv.size()-1).getVehicle_ID());
+			prepared2.setInt(2,length);
+			prepared2.setInt(3, height);
+			prepared2.setInt(4,load_limit);
+			prepared2.executeUpdate();												
 		}catch(Exception e){
 			System.err.println(e);
 		}finally {
@@ -222,23 +228,29 @@ public class VehicleDAOI implements VehicleDAO{
 			
 			Statement statement = con.createStatement();
 			statement.setQueryTimeout(60);
-			statement.executeUpdate("UPDATE vehicle SET" +
-									plate + "," +
-									color.getColor_ID() + "," +
-									model.getModel_ID() + "," +
-									manufacturer.getManufacturer_ID() + "," +
-									accessory.getAccessory_ID() + "," +
-									mileage + "," +
-									year + "," +
-									active +
-									" WHERE vehicle_ID = " + vehicle_ID + ";" );
+			String raw_query = "UPDATE vehicle SET license_plate_number= ?, manufacturer_ID=?,color_ID=?, accessory_ID=?,mileage=?, manufacture_year=?,active=? WHERE vehicle_ID =? "; 
+			PreparedStatement prepared = con.prepareStatement(raw_query);
+			prepared.setString(1, plate);
+			prepared.setInt(2, manufacturer.getManufacturer_ID());
+			prepared.setInt(3, model.getModel_ID());
+			prepared.setInt(4, color.getColor_ID());
+			prepared.setInt(5, accessory.getAccessory_ID());
+			prepared.setInt(6, mileage);
+			prepared.setInt(7, year);
+			prepared.setBoolean(8, active);
+			prepared.setInt(9, vehicle_ID);						
+			prepared.executeUpdate();		
+			
 			
 			
 			
 			statement.setQueryTimeout(60);
-			statement.executeUpdate("UPDATE car SET"+ 	doors + "," +
-														pass_limit +","+
-														" WHERE car_ID = " + vehicle_ID + ";" );
+			String raw_query2 = "UPDATE car SET doors=?, passenger_limit=? WHERE car_ID =?";
+			PreparedStatement prepared2 = con.prepareStatement(raw_query2);
+			prepared2.setInt(1, doors);
+			prepared2.setInt(2, pass_limit);
+			prepared2.setInt(3, vehicle_ID);
+			prepared2.executeUpdate();
 		}catch(Exception e){
 			System.err.println(e);
 		}finally {
@@ -258,22 +270,29 @@ public class VehicleDAOI implements VehicleDAO{
 			
 			Statement statement = con.createStatement();
 			statement.setQueryTimeout(60);
-			statement.executeUpdate("UPDATE vehicle SET" +
-									plate + "," +
-									color.getColor_ID() + "," +
-									model.getModel_ID() + "," +
-									manufacturer.getManufacturer_ID() + "," +
-									accessory.getAccessory_ID() + "," +
-									mileage + "," +
-									year + "," +
-									active +
-									" WHERE vehicle_ID = " + vehicle_ID + ";" );
+			String raw_query = "UPDATE vehicle SET license_plate_number= ?, manufacturer_ID=?, model_ID=?, color_ID=?, accessory_ID=?,mileage=?, manufacture_year=?,active=? WHERE vehicle_ID =? "; 
+			PreparedStatement prepared = con.prepareStatement(raw_query);
+			prepared.setString(1, plate);
+			prepared.setInt(2, manufacturer.getManufacturer_ID());
+			prepared.setInt(3, model.getModel_ID());
+			prepared.setInt(4, color.getColor_ID());
+			prepared.setInt(5, accessory.getAccessory_ID());
+			prepared.setInt(6, mileage);
+			prepared.setInt(7, year);
+			prepared.setBoolean(8, active);
+			prepared.setInt(9, vehicle_ID);			
+			prepared.executeUpdate();		
 			
 			
 			
 			statement.setQueryTimeout(60);
-			statement.executeUpdate("UPDATE truck SET"+ length + "," + height +","+load_limit+","+
-														" WHERE truck_ID = " + vehicle_ID + ";" );
+			String raw_query2 = "UPDATE truck SET height=?, lenght=?, loading_limit=? WHERE truck_ID =?";
+			PreparedStatement prepared2 = con.prepareStatement(raw_query2);
+			prepared2.setInt(1, height);
+			prepared2.setInt(2, length);
+			prepared2.setInt(3, load_limit);
+			prepared2.setInt(4, vehicle_ID);
+			prepared2.executeUpdate();
 		}catch(Exception e){
 			System.err.println(e);
 		}finally {
@@ -294,15 +313,26 @@ public class VehicleDAOI implements VehicleDAO{
 			
 			Statement statement = con.createStatement();
 			statement.setQueryTimeout(60);
-			statement.executeQuery("DELETE * FROM vehicle WHERE vehicle_id = " + vehicle_id + ";");
+			String raw_query = "DELETE * FROM vehicle WHERE vehicle_id = ?";
+			PreparedStatement prepared = con.prepareStatement(raw_query);
+			prepared.setInt(1, vehicle_id);
+			prepared.executeQuery();
 			Statement statement2 = con.createStatement();
 			statement2.setQueryTimeout(60);
-			statement2.executeQuery("DELETE * FROM car WHERE car_ID = " + vehicle_id + ";");
+			String raw_query1 = "DELETE * FROM car WHERE car_ID = ?";
+			PreparedStatement prepared2 = con.prepareStatement(raw_query1);
+			prepared2.setInt(1, vehicle_id);
+			prepared2.executeQuery();
 			Statement statement3 = con.createStatement();
 			statement3.setQueryTimeout(60);
-			statement3.executeQuery("DELETE * FROM truck WHERE truck_ID = " + vehicle_id + ";");
-			statement3.executeQuery("DELETE * FROM has_accessory WHERE vehicle_ID = " + vehicle_id + ";");
-		
+			String raw_query3 = "DELETE * FROM truck WHERE truck_ID = ?";
+			PreparedStatement prepared3 = con.prepareStatement(raw_query3);
+			prepared3.setInt(1, vehicle_id);
+			prepared3.executeQuery();
+			String raw_query4 ="DELETE * FROM has_accessory WHERE vehicle_ID =?";
+			PreparedStatement prepared4 = con.prepareStatement(raw_query4);
+			prepared4.setInt(1, vehicle_id);
+			prepared4.executeQuery();
 		}catch(Exception e){
 			System.err.println(e);
 		}finally {
@@ -409,8 +439,10 @@ public class VehicleDAOI implements VehicleDAO{
 			
 			Statement statement = con.createStatement();
 			statement.setQueryTimeout(60);
-			ResultSet result = statement.executeQuery("SELECT * FROM vehicle WHERE manufacturer_year = " + age + ";");
-			
+			String raw_query = "SELECT * FROM vehicle WHERE manufacturer_year = ?";
+			PreparedStatement prepared = con.prepareStatement(raw_query);
+			prepared.setInt(1, age);
+			ResultSet result = prepared.executeQuery();
 			while(result.next()){
 				Color c = col.getColorById( result.getInt("color_ID"));
 				List<Accessory> a= acc.getHasAccessory(result.getInt("vehicle_ID")); 
@@ -446,10 +478,12 @@ public class VehicleDAOI implements VehicleDAO{
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost/myimsedb?useSSL=false","root","MySQLrp");
-			
 			Statement statement = con.createStatement();
 			statement.setQueryTimeout(60);
-			ResultSet result = statement.executeQuery("SELECT * FROM vehicle WHERE color_ID = "+ color.getColor_ID()+ ";");
+			String raw_query = "SELECT * FROM vehicle WHERE color_ID = ?";
+			PreparedStatement prepared = con.prepareStatement(raw_query);
+			prepared.setInt(1, color.getColor_ID());
+			ResultSet result = prepared.executeQuery();
 			
 			while(result.next()){
 				Color c = col.getColorById( result.getInt("color_ID"));
@@ -487,8 +521,10 @@ public class VehicleDAOI implements VehicleDAO{
 			
 			Statement statement = con.createStatement();
 			statement.setQueryTimeout(60);
-			ResultSet result = statement.executeQuery("SELECT * FROM vehicle WHERE accessory_ID = "+ accessory.getAccessory_ID()+ ";");
-			
+			String raw_query = "SELECT * FROM vehicle WHERE accessory_ID = ?";
+			PreparedStatement prepared = con.prepareStatement(raw_query);
+			prepared.setInt(1,accessory.getAccessory_ID());
+			ResultSet result = prepared.executeQuery();
 			while(result.next()){
 				Color c = col.getColorById( result.getInt("color_ID"));
 				List<Accessory> a= acc.getHasAccessory(result.getInt("vehicle_ID")); 
@@ -525,7 +561,10 @@ public class VehicleDAOI implements VehicleDAO{
 			
 			Statement statement = con.createStatement();
 			statement.setQueryTimeout(60);
-			ResultSet result = statement.executeQuery("SELECT * FROM vehicle WHERE model_ID = "+ mode.getModel_ID()+ ";");
+			String raw_query = "SELECT * FROM vehicle WHERE model_ID = ?";
+			PreparedStatement prepared = con.prepareStatement(raw_query);
+			prepared.setInt(1, mode.getModel_ID());	 
+			ResultSet result = prepared.executeQuery();
 			
 			while(result.next()){
 				Color c = col.getColorById( result.getInt("color_ID"));
@@ -561,7 +600,11 @@ public class VehicleDAOI implements VehicleDAO{
 			
 			Statement statement = con.createStatement();
 			statement.setQueryTimeout(60);
-			ResultSet tresult = statement.executeQuery("SELECT * FROM truck WHERE loading_limit = "+limit+";");
+			String raw_query = "SELECT * FROM truck WHERE loading_limit = ?";
+			PreparedStatement prepared = con.prepareStatement(raw_query);
+			prepared.setInt(1, limit);
+			ResultSet tresult = prepared.executeQuery();
+			
 			while(tresult.next()){
 				Vehicle veh = v.getVehicleById(tresult.getInt("truck_ID"));
 				List<Accessory> a= acc.getHasAccessory(veh.getVehicle_ID());
@@ -593,7 +636,10 @@ public class VehicleDAOI implements VehicleDAO{
 			
 			Statement statement = con.createStatement();
 			statement.setQueryTimeout(60);
-			ResultSet tresult = statement.executeQuery("SELECT * FROM car WHERE doors = "+doors+";");
+			String raw_query = "SELECT * FROM car WHERE doors = ? ";
+			PreparedStatement prepared = con.prepareStatement(raw_query);
+			prepared.setInt(1, doors);
+			ResultSet tresult = prepared.executeQuery();
 			
 			while(tresult.next()){
 				Vehicle v = veh.getVehicleById(tresult.getInt("car_ID"));
@@ -629,7 +675,10 @@ public class VehicleDAOI implements VehicleDAO{
 			
 			Statement statement = con.createStatement();
 			statement.setQueryTimeout(60);
-			ResultSet result = statement.executeQuery("SELECT * FROM vehicle WHERE manufacturer_ID ="+ manufacturer.getManufacturer_ID()+ ";");
+			String raw_query = "SELECT * FROM vehicle WHERE manufacturer_ID = ?"; 
+			PreparedStatement prepared = con.prepareStatement(raw_query);
+			prepared.setInt(1, manufacturer.getManufacturer_ID());
+			ResultSet result = prepared.executeQuery();
 			
 			while(result.next()){
 				Color c = col.getColorById( result.getInt("color_ID"));
