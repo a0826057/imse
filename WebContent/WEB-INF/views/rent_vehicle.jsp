f076dc8d23b1ec8542623c0f74434fef5737f81b
Ines Bacic <a1349198@unet.univie.ac.at>
2018-01-08 10:48:11 +0100

    Update Use Case Rent Vehicle

--- WebContent/WEB-INF/views/rent_vehicle.jsp
+++ WebContent/WEB-INF/views/rent_vehicle.jsp
@@ -1,12 +1,67 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ page language="java" import="java.util.*, java.lang.*" %>
+<%@ page language="java" import="model.*, java.lang.*" %>
+ 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>Rent Vehicle</title>
 </head>
 <body>
-
+	<form id="rent"  action="${pageContext.request.contextPath}/RentVehicleServlet" method="get">
+         Date From (DD.MM.YYYY): <input type = "text" name = "date_from" value=<%if(session.getAttribute("date_from") != null) out.print(session.getAttribute("date_from").toString()); %>>
+         <br/>
+         Date To (DD.MM.YYYY): <input type = "text" name = "date_to"  value=<%if(session.getAttribute("date_to") != null) out.print(session.getAttribute("date_to").toString()); %>>
+         <br>
+         <input type = "submit" value = "Search" />
+     </form>
+     <br>
+     <% java.util.ArrayList<Vehicle> users = new ArrayList<Vehicle>();
+     
+    	if((session.getAttribute("list") != null) && (session.getAttribute("date_from") != null) && (session.getAttribute("date_to") != null)){
+    		users = (java.util.ArrayList<Vehicle>) session.getAttribute("list");
+    		out.print("The search result for " + session.getAttribute("date_from").toString() + "-" + session.getAttribute("date_to").toString() );
+    	}
+    		%>
+    		
+    <table border="1" cellpadding="5">
+            <tr>
+                <th>Vehicle ID</th>
+                <th>Licence Plate Number</th>
+                <th>Color</th>
+                <th>Model</th>
+                <th>Manufacturer</th>
+                <th>Accessory</th>
+                <th>Mileage</th>
+                <th>Year</th>
+                <th>Active</th>
+                <th></th>
+            </tr>
+            <%for(int i = 0; i < users.size(); i++){
+            	Vehicle veh = (Vehicle)users.get(i);
+            %>
+            <tr>
+		       <td><% out.print(veh.getVehicle_ID()); %></td>
+		       <td><% out.print(veh.getLicense_plate_number()); %></td>
+		       <td><% out.print(veh.getColor().getDescription()); %></td>
+		       <td><% out.print(veh.getModel().getDescription()); %></td>
+		       <td><% out.print(veh.getManufactur().getName()); %></td>
+		       <td>
+		       <% for(int j = 0; j < veh.getAccessory().size(); j++)
+		    	   out.print(veh.getAccessory().get(j).getName()); %>
+		       </td>
+		       <td><% out.print(veh.getMileage()); %></td>
+		       <td><% out.print(veh.getManufacture_year()); %></td>
+		       <td><% out.print(veh.getActive()); %></td>
+		       <td>
+		       <form action="${pageContext.request.contextPath}/RentVehicleServlet" method="post">
+		       		<button TYPE="submit" NAME="button" VALUE=<%=veh.getVehicle_ID()%> >Book It!</button>
+		       </form>
+		       </td>
+		    </tr>
+            <%} %>
+   	</table>
 </body>
 </html>
--- src/servlets/RentVehicleServlet.java
+++ src/servlets/RentVehicleServlet.java
@@ -1,11 +1,25 @@
 package servlets;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.CostumerDAOI;
+import dao.EmployeeDAOI;
+import dao.RentalDAOI;
+import dao.VehicleDAOI;
+import model.Rental;
+import model.Vehicle;
 
 /**
  * Servlet implementation class RentVehicleServlet
@@ -26,8 +40,45 @@ public class RentVehicleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// HERE MUST BE CHECKED IF THE USER HAS AN ACTIVE ACCOUNT AND IF HE IS LOGGED IN
+		//.............
+		
+		
+	    String date_f = request.getParameter("date_from");
+        String date_t = request.getParameter("date_to");
+        
+     
+        List<Rental> list_rentals = new ArrayList<Rental>();
+        List<Vehicle> list_vehicle = new ArrayList<Vehicle>();
+        RentalDAOI rent = new RentalDAOI();
+        VehicleDAOI veh = new VehicleDAOI();
+        SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+        try {
+			java.util.Date DateFrom  =  df.parse(date_f);
+			java.util.Date DateTo  =  df.parse(date_t);
+			list_rentals = rent.getRentalByDatePeriod(DateFrom, DateTo);
+			list_vehicle = veh.getVehicleList();
+			
+			for(Rental r : list_rentals){
+				for(int i = 0; i < list_vehicle.size(); i++){
+					if(list_vehicle.get(i).getVehicle_ID() == r.getVehicle().getVehicle_ID()){
+						list_vehicle.remove(i);
+					}
+				}
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} 
+        
+        HttpSession session = request.getSession();
+        session.setAttribute("list", list_vehicle);
+        session.setAttribute("date_from", date_f);
+        session.setAttribute("date_to", date_t);
+        
+        RequestDispatcher dispatcher = request.getRequestDispatcher("rent_vehicle.jsp");
+        
+        dispatcher.forward(request, response);
 	}
 
 	/**
@@ -35,7 +86,28 @@ public class RentVehicleServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		HttpSession session = request.getSession();
+		String button = request.getParameter("button");
+		String date_f = (String) session.getAttribute("date_from");
+        String date_t = (String) session.getAttribute("date_to");
+        
+        CostumerDAOI c = new CostumerDAOI();
+        EmployeeDAOI e = new EmployeeDAOI();
+        VehicleDAOI v = new VehicleDAOI();
+        
+        SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+		RentalDAOI rent = new RentalDAOI();
+		Rental r = null;
+		try {
+			r = new Rental(v.getVehicleById(Integer.parseInt(button)), c.getCostumerById(1), e.getEmployeeById(1), df.parse(date_f),df.parse(date_t), "not given");
+		} catch (NumberFormatException | ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		rent.addRental(r);
+		
+		response.getWriter().append("The booking was succesful!");
 	}
 
 }
