30cbaa8a10c700894967cdd98ca4f0d17f3b0dd9
Betül <betulsahin2100@gmail.com>
2018-01-22 21:11:29 +0100

    Color and Manufacturer update

--- src/dao/ColorDAOM.java
+++ src/dao/ColorDAOM.java
@@ -1,33 +1,25 @@
 package dao;
 
-import model.Accessory;
+
 import model.Color;
 
-import java.net.UnknownHostException;
-import java.sql.*;
+
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+
 import java.util.List;
-import java.util.Set;
+
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
-import com.mongodb.BasicDBList;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
-import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
+
 import com.mongodb.client.MongoDatabase;
 
-import jdk.nashorn.internal.ir.Flags;
+
 
 //written by a01349198 - IB
 //MongoDB
@@ -37,164 +29,122 @@ public class ColorDAOM implements ColorDAO{
 	
 	@Override
 	public List<Color> getColorList(){
-		colors = new ArrayList<Color>();
-		client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
-		MongoDatabase database = client.getDatabase("imse"); 
-		MongoCollection<Document> collection = database.getCollection("Color");
-		System.out.println("before find");
-		List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
-		System.out.println("after find");
-		if(foundDocument==null) {
-			System.out.println("it is empty");
+		try{
+			colors = new ArrayList<Color>();
+			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+			MongoDatabase database = client.getDatabase("imse"); 
+			MongoCollection<Document> collection = database.getCollection("color");
+			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
+				for (int i = 0; i < foundDocument.size(); i++) {
+					Document col = foundDocument.get(i);
+					String description = (String) col.get("description");
+					String color_code = (String) col.get("manufacturer_color_code");
+					Color data = new Color(i,description,color_code);
+					colors.add(data);
+				}
+		}catch (MongoException e){
+		    System.out.println(e.getClass().getCanonicalName());
+		}finally {
+			client.close();
 		}
-		Boolean flag=false;
-			for (int i = 0; i < foundDocument.size(); i++) {
-				System.out.println("inside the loop size");
-				Document col = foundDocument.get(i);
-				String description = (String) col.get("description");
-				String color_code = (String) col.get("manufacturer_color_code");
-				Color data = new Color(i,description,color_code);
-				colors.add(data);
-				flag=true;
-			}
-			
-		if(flag!=false) {
-			client.close();		
-		} 
 			return colors;
 	}
+	
 	@Override
 	public Color getColorById(int color_id){
-		/*	Color co = null;
-		try{
-		
-		client = new MongoClient("localhost", 27017);
-		MongoDatabase database = client.getDatabase("imse");
-		DBCollection collection = (DBCollection) database.getCollection("Color");
-		BasicDBObject query = new BasicDBObject();
-		String id= String.valueOf(color_id);
-		query.put(id, 1);
-		DBCursor cursor = collection.find(query);
-		while (cursor.hasNext()) {
-			colors.add((Color) cursor.next());
-		}
-		
-		}catch(Exception e){
-			System.err.println(e);
-		}finally {
-			
-		}
-		*/
+		//manufacturer_ID not applicable for mongoDB
 		return null;
 	}
 	
 	@Override
 	public void addColor(String description, String manufacturer_color_code){
-	
-	
-		client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+		try{
+			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
 			MongoDatabase database = client.getDatabase("imse");
-			MongoCollection<Document>  collection = database.getCollection("Color");
+			MongoCollection<Document>  collection = database.getCollection("color");
 			Document documents = new Document();
 			documents.put("desciption",description);
 			documents.put("manufacturer_color_code", manufacturer_color_code);
 			collection.insertOne(documents);
+		}catch (MongoException e){
+		    System.out.println(e.getClass().getCanonicalName());
+		}finally {
 			client.close();
+		}
 		
 	}
 	
 	@Override
 	public void changeColor(int color_ID, String description, String manufacturer_color_code){
-				
+		Bson filter;
+		Bson newValue;
+		try{		
 			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
 			MongoDatabase database = client.getDatabase("imse"); 
-			MongoCollection<Document> collection = database.getCollection("Color");
-			Bson filter = new Document("manufacturer_color_code", manufacturer_color_code);
-			Bson newValue = new Document("description", description);
+			MongoCollection<Document> collection = database.getCollection("color");
+			filter = new Document("manufacturer_color_code", manufacturer_color_code);
+			newValue = new Document("description", description);
 			Bson updateOperationDocument = new Document("$set", newValue);
 			collection.updateOne(filter, updateOperationDocument);
+		}catch (MongoException e){
+		    System.out.println(e.getClass().getCanonicalName());
+		}finally {
 			client.close();
-				
+		}
 	}
 	
 	@Override
 	public void deleteColor(int color_id){
-		/*Connection con = null;
-		try{
-			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost/myimsedb?useSSL=false","root","MySQLrp");
-			
-			Statement statement = con.createStatement();
-			statement.setQueryTimeout(60);
-			statement.executeQuery("DELETE * FROM color WHERE color_ID = " + color_id + ";");
-		
-		}catch(Exception e){
-			System.err.println(e);
-		}finally {
-			try {
-				if (con != null)
-					con.close();
-			}catch (SQLException e) {
-				System.err.println(e);
-			}
-		}*/
+		//color_ID not applicable for mongoDB
 	}
 	
 	@Override
 	public int getColorCount(){
-		colors = new ArrayList<Color>();
-		client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
-		MongoDatabase database = client.getDatabase("imse"); 
-		MongoCollection<Document> collection = database.getCollection("Color");
-		List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
-		if(foundDocument==null) {
-			System.out.println("it is empty");
-		}
-		Boolean flag=false;
-			for (int i = 0; i < foundDocument.size(); i++) {
-				Document col = foundDocument.get(i);
-				String description = (String) col.get("description");
-				String color_code = (String) col.get("manufacturer_color_code");
-				Color data = new Color(i,description,color_code);
-				colors.add(data);
-				flag=true;
-			}
-			
-		if(flag!=false) {
-			client.close();		
-		} 
-		
+		try{
+			colors = new ArrayList<Color>();
+			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+			MongoDatabase database = client.getDatabase("imse"); 
+			MongoCollection<Document> collection = database.getCollection("color");
+			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
+				for (int i = 0; i < foundDocument.size(); i++) {
+					Document col = foundDocument.get(i);
+					String description = (String) col.get("description");
+					String color_code = (String) col.get("manufacturer_color_code");
+					Color data = new Color(i,description,color_code);
+					colors.add(data);
+				}
+		}catch (MongoException e){
+		    System.out.println(e.getClass().getCanonicalName());
+		}finally {
+			client.close();
+		}		
 		return colors.size();
 	}
 	
 	@Override
 	public List<Color> getColorsByDescription(String description){
 		colors = new ArrayList<Color>();
-		client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
-		MongoDatabase database = client.getDatabase("imse"); 
-		MongoCollection<Document> collection = database.getCollection("Color");
-		
-		List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
-		
-		if(foundDocument==null) {
-			System.out.println("it is empty");
-		}
-		Boolean flag=false;
-			for (int i = 0; i < foundDocument.size(); i++) {
-				Document col = foundDocument.get(i);
-				String descriptiond = (String) col.get("description");
-				if(descriptiond.equals(description)) {
-					String color_code = (String) col.get("manufacturer_color_code");
-					Color data = new Color(i,descriptiond,color_code);
-					colors.add(data);
-					flag=true;
+		try{
+			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+			MongoDatabase database = client.getDatabase("imse"); 
+			MongoCollection<Document> collection = database.getCollection("color");
+			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
+				for (int i = 0; i < foundDocument.size(); i++) {
+					Document col = foundDocument.get(i);
+					String descriptiond = (String) col.get("description");
+					if(descriptiond.equals(description)) {
+						String color_code = (String) col.get("manufacturer_color_code");
+						Color data = new Color(i,descriptiond,color_code);
+						colors.add(data);
+						
+					}
 				}
-				
-			}
 			
-		if(flag!=false) {
-			client.close();		
-		} 
+		}catch (MongoException e){
+		    System.out.println(e.getClass().getCanonicalName());
+		}finally {
+			client.close();
+		}
 		return colors;
 	}
 }
--- src/dao/ManufacturerDAOM.java
+++ src/dao/ManufacturerDAOM.java
@@ -1,9 +1,8 @@
 package dao;
 
-import model.Color;
+
 import model.Manufacturer;
 
-import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +11,7 @@ import org.bson.conversions.Bson;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -19,155 +19,126 @@ import com.mongodb.client.MongoDatabase;
 
 public class ManufacturerDAOM implements ManufacturerDAO{
 	private List<Manufacturer> manufacturers;
+	private MongoClient mongoClient;
 	private MongoClient client;
 	@Override
 	public List<Manufacturer> getManufacturerList(){
-		manufacturers = new ArrayList<Manufacturer>();
-		client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
-		MongoDatabase database = client.getDatabase("imse"); 
-		MongoCollection<Document> collection = database.getCollection("Manufacturer");
-		List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
-		
-		if(foundDocument==null) {
-			System.out.println("it is empty");
+		try{
+			manufacturers = new ArrayList<Manufacturer>();
+			mongoClient = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+			MongoDatabase database = mongoClient.getDatabase("imse"); 
+			MongoCollection<Document> collection = database.getCollection("manufacturer");
+			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
+				for (int i = 0; i < foundDocument.size(); i++) {
+					Document col = foundDocument.get(i);
+					String name = (String) col.get("name");
+					String country = (String) col.get("country");
+					Manufacturer data = new Manufacturer(i,name,country);
+					manufacturers.add(data);
+				}
+		}catch (MongoException e){
+		    System.out.println(e.getClass().getCanonicalName());
+		}finally {
+			client.close();
 		}
-		Boolean flag=false;
-			for (int i = 0; i < foundDocument.size(); i++) {
-			
-				Document col = foundDocument.get(i);
-				String name = (String) col.get("name");
-				String country = (String) col.get("country");
-				Manufacturer data = new Manufacturer(i,name,country);
-				manufacturers.add(data);
-				flag=true;
-			}
-			
-		if(flag!=false) {
-			client.close();		
-		} 
 		
 		return manufacturers;
 	}
 	
 	@Override
 	public Manufacturer getManufacturerById(int manufacturer_id){
-		/*Connection con = null;
-		Manufacturer ac = null;
-		try{
-			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost/myimsedb?useSSL=false","root","MySQLrp");
-			
-			Statement statement = con.createStatement();
-			statement.setQueryTimeout(60);
-			String raw_query = "SELECT * FROM manufacturer WHERE manufacturer_ID = ?;";
-			PreparedStatement prepared = con.prepareStatement(raw_query);
-			prepared.setInt(1, manufacturer_id);
-			ResultSet result = prepared.executeQuery();
-			
-			while(result.next()){
-				 ac = new Manufacturer(result.getInt("manufacturer_ID"), result.getString("name"), result.getString("country"));
-			}
-		
-		}catch(Exception e){
-			System.err.println(e);
-		}finally {
-			try {
-				if (con != null)
-					con.close();
-			}catch (SQLException e) {
-				System.err.println(e);
-			}
-		}*/
+		//manufacturer_ID not applicable for mongoDB
 		return null;
 	}
 	
 	@Override
 	public void addManufacturer(String name, String country){
-		client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
-		MongoDatabase database = client.getDatabase("imse");
-		MongoCollection<Document>  collection = database.getCollection("Manufacturer");
-		Document documents = new Document();
-		documents.put("name",name);
-		documents.put("country", country);
-		collection.insertOne(documents);
-		client.close();
+		try{
+			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+			MongoDatabase database = client.getDatabase("imse");
+			MongoCollection<Document>  collection = database.getCollection("manufacturer");
+			Document documents = new Document();
+			documents.put("name",name);
+			documents.put("country", country);
+			collection.insertOne(documents);
+			
+		}catch (MongoException e){
+		    System.out.println(e.getClass().getCanonicalName());
+		}finally {
+			client.close();
+		}
 	}
 	
 	@Override
 	public void changeManufacturer(int manufacturer_ID, String name, String country){
-		client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
-		MongoDatabase database = client.getDatabase("imse"); 
-		MongoCollection<Document> collection = database.getCollection("Manufacturer");
-		Bson filter = new Document("name", name);
-		Bson newValue = new Document("country", country);
-		Bson updateOperationDocument = new Document("$set", newValue);
-		collection.updateOne(filter, updateOperationDocument);
-		client.close();
+		Bson filter;
+		Bson newValue;
+		try{
+			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+			MongoDatabase database = client.getDatabase("imse"); 
+			MongoCollection<Document> collection = database.getCollection("manufacturer");
+			if(manufacturer_ID==1) {
+				//if id==1 change by name
+				 filter = new Document("name", name);
+				 newValue = new Document("country", country);
+				
+			}else {
+				//if id!=1 change by country
+				 filter = new Document("country", country);
+				 newValue = new Document("name", name);			
+			}
+			Bson updateOperationDocument = new Document("$set", newValue);
+			collection.updateOne(filter, updateOperationDocument);
+		}catch (MongoException e){
+		    System.out.println(e.getClass().getCanonicalName());
+		}finally {
+			client.close();
+		}
 	}
 	
 	@Override
 	public void deleteManufacturer(int manufacturer_ID){
-		/*Connection con = null;
-		try{
-			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost/myimsedb?useSSL=false","root","MySQLrp");
-			
-			Statement statement = con.createStatement();
-			statement.setQueryTimeout(60);
-			String raw_query = "DELETE * FROM manufacturer WHERE manufacturer_ID = ?;";
-			PreparedStatement prepared = con.prepareStatement(raw_query);
-			prepared.setInt(1, manufacturer_ID);
-			prepared.executeQuery();
-		
-		}catch(Exception e){
-			System.err.println(e);
-		}finally {
-			try {
-				if (con != null)
-					con.close();
-			}catch (SQLException e) {
-				System.err.println(e);
-			}
-		}*/
+		//manufacturer_ID not applicable for mongoDB
 	}
 	
+		
+
 	@Override
 	public int getManufacturerCount(){
-		manufacturers = new ArrayList<Manufacturer>();
-		client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
-		MongoDatabase database = client.getDatabase("imse"); 
-		MongoCollection<Document> collection = database.getCollection("Color");
-		List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
-		if(foundDocument==null) {
-			System.out.println("it is empty");
+		try{
+			manufacturers = new ArrayList<Manufacturer>();
+			//db.runCommand({whatsmyuri : 1})
+			mongoClient = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+			MongoDatabase database = mongoClient.getDatabase("imse"); 
+			MongoCollection<Document> collection = database.getCollection("manufacturer");
+			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
+			System.out.println(foundDocument.size());
+				for (int i = 0; i < foundDocument.size(); i++) {
+					Document col = foundDocument.get(i);
+					String name = (String) col.get("name");
+					String country = (String) col.get("country");
+					Manufacturer data = new Manufacturer(i,name,country);
+					manufacturers.add(data);
+		
+				}
+		}catch (MongoException e){
+		    System.out.println(e.getClass().getCanonicalName());
+		}finally {
+			client.close();
 		}
-		Boolean flag=false;
-			for (int i = 0; i < foundDocument.size(); i++) {
-				Document col = foundDocument.get(i);
-				String name = (String) col.get("name");
-				String country = (String) col.get("country");
-				Manufacturer data = new Manufacturer(i,name,country);
-				manufacturers.add(data);
-				flag=true;
-			}
-			
-		if(flag!=false) {
-			client.close();		
-		} 
 		return manufacturers.size();
 	}
 	
 	@Override
 	public Manufacturer getManufacturerByName(String name){
 		Manufacturer ac = null;
-		client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
-		MongoDatabase database = client.getDatabase("imse"); 
-		MongoCollection<Document> collection = database.getCollection("Color");
-		List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
-		if(foundDocument==null) {
-			System.out.println("it is empty");
-		}
-		Boolean flag=false;
+		try{
+			manufacturers = new ArrayList<Manufacturer>();
+			mongoClient = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+			MongoDatabase database = mongoClient.getDatabase("imse"); 
+			MongoCollection<Document> collection = database.getCollection("manufacturer");
+			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
+			
 			for (int i = 0; i < foundDocument.size(); i++) {
 				Document col = foundDocument.get(i);
 				String names = (String) col.get("name");
@@ -175,27 +146,26 @@ public class ManufacturerDAOM implements ManufacturerDAO{
 					String country = (String) col.get("country");
 					ac = new Manufacturer(i,names,country);
 				}
-							
-				flag=true;
-			}
+			}		
+		}catch (MongoException e){
+		    System.out.println(e.getClass().getCanonicalName());
+		}finally {
+			client.close();
+		}	
 			
-		if(flag!=false) {
-			client.close();		
-		} 
 		return ac;
 	}
 	
 	public List<Manufacturer> getManufacturersByCountry(String country){
 		manufacturers = new ArrayList<Manufacturer>();
 		Manufacturer ac = null;
-		client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
-		MongoDatabase database = client.getDatabase("imse"); 
-		MongoCollection<Document> collection = database.getCollection("Color");
-		List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
-		if(foundDocument==null) {
-			System.out.println("it is empty");
-		}
-		Boolean flag=false;
+		try{
+			manufacturers = new ArrayList<Manufacturer>();
+			mongoClient = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+			MongoDatabase database = mongoClient.getDatabase("imse"); 
+			MongoCollection<Document> collection = database.getCollection("manufacturer");
+			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
+			
 			for (int i = 0; i < foundDocument.size(); i++) {
 				Document col = foundDocument.get(i);
 				String name = (String) col.get("name");
@@ -204,13 +174,13 @@ public class ManufacturerDAOM implements ManufacturerDAO{
 					ac = new Manufacturer(i,name,country);
 					manufacturers.add(ac);
 				}
-							
-				flag=true;
-			}
+			}		
+		}catch (MongoException e){
+		    System.out.println(e.getClass().getCanonicalName());
+		}finally {
+			client.close();
+		}	
 			
-		if(flag!=false) {
-			client.close();		
-		} 
 		return manufacturers;
 	}
 }
