ff92ac2cabd9c224008ec10c62f030216c0be1d7
Ines Bacic <a1349198@unet.univie.ac.at>
2018-01-24 00:06:12 +0100

    ManufacturerDAOM Impemented & Tested (total functionality working)

--- src/dao/ManufacturerDAOM.java
+++ src/dao/ManufacturerDAOM.java
@@ -1,14 +1,9 @@
 package dao;
 
-
 import model.Manufacturer;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.bson.Document;
-import org.bson.conversions.Bson;
-
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.MongoException;
@@ -21,45 +16,76 @@ public class ManufacturerDAOM implements ManufacturerDAO{
 	private List<Manufacturer> manufacturers;
 	private MongoClient mongoClient;
 	private MongoClient client;
+	
 	@Override
 	public List<Manufacturer> getManufacturerList(){
+		
 		try{
 			manufacturers = new ArrayList<Manufacturer>();
 			mongoClient = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
 			MongoDatabase database = mongoClient.getDatabase("imse"); 
-			MongoCollection<Document> collection = database.getCollection("manufacturer");
+			MongoCollection<Document> collection = database.getCollection("imse.manufacturer");
 			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
-				for (int i = 0; i < foundDocument.size(); i++) {
-					Document col = foundDocument.get(i);
-					String name = (String) col.get("name");
-					String country = (String) col.get("country");
-					Manufacturer data = new Manufacturer(i,name,country);
-					manufacturers.add(data);
-				}
+			
+			for (int i = 0; i < foundDocument.size(); i++) {
+				Document col = foundDocument.get(i);
+				String id_str = col.getString("manufacturer_id");
+				int id = Integer.parseInt(id_str);
+				String name = (String) col.get("name");
+				String country = (String) col.get("country");
+				Manufacturer data = new Manufacturer(id,name,country);
+				manufacturers.add(data);
+			}
 		}catch (MongoException e){
 		    System.out.println(e.getClass().getCanonicalName());
 		}finally {
-			client.close();
+			if(client != null)
+				client.close();
 		}
 		
 		return manufacturers;
 	}
-	
+
 	@Override
 	public Manufacturer getManufacturerById(int manufacturer_id){
-		//manufacturer_ID not applicable for mongoDB
-		return null;
+		manufacturers = new ArrayList<Manufacturer>();
+		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+		MongoDatabase database = mongoClient.getDatabase("imse"); 
+		
+		MongoCollection<Document> coll = database.getCollection("imse.manufacturer");
+		Document searchQuery = new Document("manufacturer_id", Integer.toString(manufacturer_id));
+		Document result = null;
+		
+		for(Document ac : coll.find(searchQuery))
+			result = ac;
+		
+		String id_str = (String) result.get("manufacturer_id");
+		int id = Integer.parseInt(id_str);
+		String name = (String) result.get("name");
+		String country = (String) result.get("country");
+		Manufacturer m = new Manufacturer(id,name,country);
+		
+		if (mongoClient != null)
+			mongoClient.close();
+		
+		return m;
 	}
-	
+
 	@Override
 	public void addManufacturer(String name, String country){
 		try{
 			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
 			MongoDatabase database = client.getDatabase("imse");
-			MongoCollection<Document>  collection = database.getCollection("manufacturer");
+			MongoCollection<Document>  collection = database.getCollection("imse.manufacturer");
+			List<Document> accs = collection.find().into(new ArrayList<Document>());
+			String id_string = (String)accs.get((accs.size()-1)).get("manufacturer_id");
+			int id = Integer.parseInt(id_string) + 1;
+			
 			Document documents = new Document();
+			documents.put("manufacturer_id", Integer.toString(id));
 			documents.put("name",name);
 			documents.put("country", country);
+			
 			collection.insertOne(documents);
 			
 		}catch (MongoException e){
@@ -68,64 +94,67 @@ public class ManufacturerDAOM implements ManufacturerDAO{
 			client.close();
 		}
 	}
-	
+
 	@Override
 	public void changeManufacturer(int manufacturer_ID, String name, String country){
-		Bson filter;
-		Bson newValue;
 		try{
 			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
 			MongoDatabase database = client.getDatabase("imse"); 
-			MongoCollection<Document> collection = database.getCollection("manufacturer");
-			if(manufacturer_ID==1) {
-				//if id==1 change by name
-				 filter = new Document("name", name);
-				 newValue = new Document("country", country);
-				
-			}else {
-				//if id!=1 change by country
-				 filter = new Document("country", country);
-				 newValue = new Document("name", name);			
-			}
-			Bson updateOperationDocument = new Document("$set", newValue);
-			collection.updateOne(filter, updateOperationDocument);
+			MongoCollection<Document> collection = database.getCollection("imse.manufacturer");
+			
+			Document searchQuery = new Document("manufacturer_id", Integer.toString(manufacturer_ID));
+			Document newValues = new Document();
+			newValues.put("name", name);
+			newValues.put("country", country);
+			
+			Document update = new Document("$set", newValues);
+			collection.updateOne(searchQuery, update);
 		}catch (MongoException e){
 		    System.out.println(e.getClass().getCanonicalName());
 		}finally {
 			client.close();
 		}
 	}
-	
+
 	@Override
 	public void deleteManufacturer(int manufacturer_ID){
-		//manufacturer_ID not applicable for mongoDB
+		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+		MongoDatabase database = mongoClient.getDatabase("imse"); 
+		
+		MongoCollection<Document> coll = database.getCollection("imse.manufacturer");
+		Document query = new Document("manufacturer_id", Integer.toString(manufacturer_ID));
+		
+		coll.findOneAndDelete(query);
+			
+		if (mongoClient != null)
+			mongoClient.close();
 	}
 	
-		
-
 	@Override
 	public int getManufacturerCount(){
 		try{
 			manufacturers = new ArrayList<Manufacturer>();
-			//db.runCommand({whatsmyuri : 1})
 			mongoClient = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
 			MongoDatabase database = mongoClient.getDatabase("imse"); 
-			MongoCollection<Document> collection = database.getCollection("manufacturer");
+			MongoCollection<Document> collection = database.getCollection("imse.manufacturer");
 			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
-			System.out.println(foundDocument.size());
-				for (int i = 0; i < foundDocument.size(); i++) {
-					Document col = foundDocument.get(i);
-					String name = (String) col.get("name");
-					String country = (String) col.get("country");
-					Manufacturer data = new Manufacturer(i,name,country);
-					manufacturers.add(data);
-		
-				}
+			
+			for (int i = 0; i < foundDocument.size(); i++) {
+				Document col = foundDocument.get(i);
+				String id_str = col.getString("manufacturer_id");
+				int id = Integer.parseInt(id_str);
+				String name = (String) col.get("name");
+				String country = (String) col.get("country");
+				Manufacturer data = new Manufacturer(id,name,country);
+				manufacturers.add(data);
+			}
 		}catch (MongoException e){
 		    System.out.println(e.getClass().getCanonicalName());
 		}finally {
-			client.close();
+			if(client != null)
+				client.close();
 		}
+		
 		return manufacturers.size();
 	}
 	
@@ -136,26 +165,30 @@ public class ManufacturerDAOM implements ManufacturerDAO{
 			manufacturers = new ArrayList<Manufacturer>();
 			mongoClient = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
 			MongoDatabase database = mongoClient.getDatabase("imse"); 
-			MongoCollection<Document> collection = database.getCollection("manufacturer");
+			MongoCollection<Document> collection = database.getCollection("imse.manufacturer");
 			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
 			
 			for (int i = 0; i < foundDocument.size(); i++) {
 				Document col = foundDocument.get(i);
+				String id_str = (String)col.getString("manufacturer_id");
+				int id = Integer.parseInt(id_str);
 				String names = (String) col.get("name");
 				if(names.equals(name)) {
 					String country = (String) col.get("country");
-					ac = new Manufacturer(i,names,country);
+					ac = new Manufacturer(id,names,country);
 				}
 			}		
 		}catch (MongoException e){
 		    System.out.println(e.getClass().getCanonicalName());
 		}finally {
-			client.close();
+			if(client != null)
+				client.close();
 		}	
 			
+		
 		return ac;
 	}
-	
+
 	public List<Manufacturer> getManufacturersByCountry(String country){
 		manufacturers = new ArrayList<Manufacturer>();
 		Manufacturer ac = null;
@@ -163,22 +196,25 @@ public class ManufacturerDAOM implements ManufacturerDAO{
 			manufacturers = new ArrayList<Manufacturer>();
 			mongoClient = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
 			MongoDatabase database = mongoClient.getDatabase("imse"); 
-			MongoCollection<Document> collection = database.getCollection("manufacturer");
+			MongoCollection<Document> collection = database.getCollection("imse.manufacturer");
 			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
 			
 			for (int i = 0; i < foundDocument.size(); i++) {
 				Document col = foundDocument.get(i);
+				String id_str = (String)col.getString("manufacturer_id");
+				int id = Integer.parseInt(id_str);
 				String name = (String) col.get("name");
 				String countries = (String) col.get("country");
 				if(countries.equals(country)) {
-					ac = new Manufacturer(i,name,country);
+					ac = new Manufacturer(id,name,country);
 					manufacturers.add(ac);
 				}
 			}		
 		}catch (MongoException e){
 		    System.out.println(e.getClass().getCanonicalName());
 		}finally {
-			client.close();
+			if(client != null)
+				client.close();
 		}	
 			
 		return manufacturers;
