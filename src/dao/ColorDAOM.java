e66a113c0f05a4af6667ce6ea5bedb7a60664980
Betül <betulsahin2100@gmail.com>
2018-01-24 02:31:17 +0100

    Finish ColorDAOM

--- src/dao/ColorDAOM.java
+++ src/dao/ColorDAOM.java
@@ -21,7 +21,7 @@ import com.mongodb.client.MongoDatabase;
 
 
 
-//written by a01349198 - IB
+
 //MongoDB
 public class ColorDAOM implements ColorDAO{
 	private List<Color> colors;
@@ -37,9 +37,11 @@ public class ColorDAOM implements ColorDAO{
 			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
 				for (int i = 0; i < foundDocument.size(); i++) {
 					Document col = foundDocument.get(i);
+					String idMongo = (String) col.get("color_id");
+					int id = Integer.valueOf(idMongo);
 					String description = (String) col.get("description");
 					String color_code = (String) col.get("manufacturer_color_code");
-					Color data = new Color(i,description,color_code);
+					Color data = new Color(id,description,color_code);
 					colors.add(data);
 				}
 		}catch (MongoException e){
@@ -52,8 +54,30 @@ public class ColorDAOM implements ColorDAO{
 	
 	@Override
 	public Color getColorById(int color_id){
-		//manufacturer_ID not applicable for mongoDB
-		return null;
+		Color colorId = null;
+		try{
+			colors = new ArrayList<Color>();
+			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+			MongoDatabase database = client.getDatabase("imse"); 
+			MongoCollection<Document> collection = database.getCollection("color");
+			Document query = new Document("color_id",Integer.toString(color_id));
+			Document result = null;
+			for(Document color : collection.find(query)) {
+				result = color;
+			}
+			String idMongo = (String) result.get("color_id");
+			int id = Integer.valueOf(idMongo);
+			String description = (String) result.get("description");
+			String color_code = (String) result.get("manufacturer_color_code");	
+			colorId = new Color(id, description, color_code);
+			
+		}catch (MongoException e){
+		    System.out.println(e.getClass().getCanonicalName());
+		}finally {
+			client.close();
+		}
+		return colorId;
+	
 	}
 	
 	@Override
@@ -61,8 +85,13 @@ public class ColorDAOM implements ColorDAO{
 		try{
 			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
 			MongoDatabase database = client.getDatabase("imse");
-			MongoCollection<Document>  collection = database.getCollection("color");
+			MongoCollection<Document> collection = database.getCollection("color");
+			List<Document> col = collection.find().into(new ArrayList<Document>());
+			String id_string = (String)col.get((col.size()-1)).get("color_id");
+			int id = Integer.parseInt(id_string) + 1;
+			
 			Document documents = new Document();
+			documents.put("color_id", Integer.toString(id));
 			documents.put("desciption",description);
 			documents.put("manufacturer_color_code", manufacturer_color_code);
 			collection.insertOne(documents);
@@ -76,16 +105,18 @@ public class ColorDAOM implements ColorDAO{
 	
 	@Override
 	public void changeColor(int color_ID, String description, String manufacturer_color_code){
-		Bson filter;
+		Bson filter ;
 		Bson newValue;
-		try{		
+		try{
+			
 			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
 			MongoDatabase database = client.getDatabase("imse"); 
 			MongoCollection<Document> collection = database.getCollection("color");
-			filter = new Document("manufacturer_color_code", manufacturer_color_code);
+			filter = new Document("color_id", Integer.toString(color_ID));
 			newValue = new Document("description", description);
-			Bson updateOperationDocument = new Document("$set", newValue);
-			collection.updateOne(filter, updateOperationDocument);
+			//newValue = new Document("manufacturer_color_code", manufacturer_color_code);
+			Bson update = new Document("$set", newValue);
+			collection.updateOne(filter, update);
 		}catch (MongoException e){
 		    System.out.println(e.getClass().getCanonicalName());
 		}finally {
@@ -95,7 +126,17 @@ public class ColorDAOM implements ColorDAO{
 	
 	@Override
 	public void deleteColor(int color_id){
-		//color_ID not applicable for mongoDB
+		try{		
+			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+			MongoDatabase database = client.getDatabase("imse"); 
+			MongoCollection<Document> collection = database.getCollection("color");
+			Document query = new Document("color_id", Integer.toString(color_id));
+			collection.findOneAndDelete(query);
+		}catch (MongoException e){
+		    System.out.println(e.getClass().getCanonicalName());
+		}finally {
+			client.close();
+		}
 	}
 	
 	@Override
@@ -108,9 +149,11 @@ public class ColorDAOM implements ColorDAO{
 			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
 				for (int i = 0; i < foundDocument.size(); i++) {
 					Document col = foundDocument.get(i);
+					String id_str = (String) col.get("color_id");
+					int id = Integer.parseInt(id_str);
 					String description = (String) col.get("description");
 					String color_code = (String) col.get("manufacturer_color_code");
-					Color data = new Color(i,description,color_code);
+					Color data = new Color(id,description,color_code);
 					colors.add(data);
 				}
 		}catch (MongoException e){
@@ -128,16 +171,16 @@ public class ColorDAOM implements ColorDAO{
 			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
 			MongoDatabase database = client.getDatabase("imse"); 
 			MongoCollection<Document> collection = database.getCollection("color");
-			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
+			Document query = new Document("description",description);
+			List<Document> foundDocument = collection.find(query).into(new ArrayList<Document>());
 				for (int i = 0; i < foundDocument.size(); i++) {
 					Document col = foundDocument.get(i);
+					String id_str = (String) col.get("color_id");
+					int id = Integer.parseInt(id_str);
 					String descriptiond = (String) col.get("description");
-					if(descriptiond.equals(description)) {
-						String color_code = (String) col.get("manufacturer_color_code");
-						Color data = new Color(i,descriptiond,color_code);
-						colors.add(data);
-						
-					}
+					String color_code = (String) col.get("manufacturer_color_code");
+					Color data = new Color(id,descriptiond,color_code);
+					colors.add(data);
 				}
 			
 		}catch (MongoException e){
