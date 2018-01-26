c270dbdb758a69adbf1cba800ad90b611e6191e7
Betül <betulsahin2100@gmail.com>
2018-01-26 22:08:57 +0100

    Vehicle - Add truck/ add car

--- src/dao/VehicleDAOM.java
+++ src/dao/VehicleDAOM.java
@@ -188,9 +188,8 @@ public class VehicleDAOM implements VehicleDAO{
 				Accessory a = new Accessory(ida, aName, descriptionA);
 				acc.add(a);
 			}
-			
+		
 			vehicl = new Vehicle(id, license_plate, colD, mo, manufa, acc, mileage, manufacturer_year, active);
-			
 			
 		}catch (MongoException e){
 		    System.out.println(e.getClass().getCanonicalName());
@@ -293,9 +292,63 @@ public class VehicleDAOM implements VehicleDAO{
 	}
 	
 	public void addCar(String plate, int color, int model, int manufacturer, int accessory, int mileage, int year, Boolean active, int doors, int pass_limit){
-		
+			ColorDAOM cols = new ColorDAOM();
+			ArrayList<Color> co = new ArrayList<Color>();
+			ModelDAOM mods = new ModelDAOM();
+			ManufacturerDAOM mans = new ManufacturerDAOM();
+			AccessoryDAOM acce = new AccessoryDAOM();
 		try{
-			//Add
+			client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+			MongoDatabase database = client.getDatabase("imse"); 
+			MongoCollection<Document> collection = database.getCollection("vehicle");
+			List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
+			
+			Document documents = new Document();
+			Document mod = new Document();
+			Document col = new Document();
+			Document man = new Document();
+			Document acc = new Document();
+			String id_veh = (String)foundDocument.get((foundDocument.size()-1)).get("vehicle_id");
+			int idv = Integer.parseInt(id_veh) + 1;
+			documents.put("vehicle_id", Integer.toString(idv));
+			documents.put("license_plate_number", plate);
+			//color
+			String mId = Integer.toString(cols.getColorById(color).getColor_ID());
+			col.put("color_id", mId); 
+			col.put("description", cols.getColorById(color).getDescription());
+			col.put("manufacturer_color_code", cols.getColorById(color).getManufacturer_color_code());
+			documents.put("color", col);
+		
+			//model
+			String moId = Integer.toString(mods.getModelById(model).getModel_ID());
+			mod.put("model_id", moId);
+			//manufacturer
+			String id_str1 =Integer.toString(mans.getManufacturerById(manufacturer).getManufacturer_ID());
+			man.put("manufacturer_id", id_str1); 
+			man.put("name",mans.getManufacturerById(manufacturer).getName());
+			man.put("country",mans.getManufacturerById(manufacturer).getCountry());
+			mod.put("manufacturer", man);
+			String mop =Double.toString(mods.getModelById(model).getPrice());
+			mod.put("price",mop);
+			documents.put("model", mod);
+			//accessory
+			String ida =Integer.toString(acce.getAccessoryById(accessory).getAccessory_ID());
+			acc.put("accessory_id", ida);
+			acc.put("name",acce.getAccessoryById(accessory).getName());
+			acc.put("name",acce.getAccessoryById(accessory).getDescription());
+			documents.put("accessory", acc);
+			String mil = Integer.toString(mileage);
+			documents.put("mileage", mil);
+			String myear = Integer.toString(year);
+			documents.put("manufacturer_year", myear);
+			documents.put("type", "car");
+			String bool = Boolean.toString(active);
+			documents.put("active", bool);
+			String dd = Integer.toString(doors);
+			documents.put("doors", dd);
+			String pp = Integer.toString(pass_limit);
+			documents.put("passenger_limit", pp);
+			collection.insertOne(documents);			
 		
 		}catch (MongoException e){
 		    System.out.println(e.getClass().getCanonicalName());
@@ -305,14 +358,79 @@ public class VehicleDAOM implements VehicleDAO{
 	}
 	
 	public void addTruck(String plate, int color, int model, int manufacturer, int accessory, int mileage, int year, Boolean active, int length, int height, int load_limit){
-		try{
-			//Add
+		ColorDAOM cols = new ColorDAOM();
+		ArrayList<Color> co = new ArrayList<Color>();
+		ModelDAOM mods = new ModelDAOM();
+		ManufacturerDAOM mans = new ManufacturerDAOM();
+		AccessoryDAOM acce = new AccessoryDAOM();
+	try{
+		client = new MongoClient(new MongoClientURI("mongodb://127.0.0.1:27017"));
+		MongoDatabase database = client.getDatabase("imse"); 
+		MongoCollection<Document> collection = database.getCollection("vehicle");
+		List<Document> foundDocument = collection.find().into(new ArrayList<Document>());
 		
-		}catch (MongoException e){
-		    System.out.println(e.getClass().getCanonicalName());
-		}finally {
-			client.close();
-		}
+		Document documents = new Document();
+		Document mod = new Document();
+		Document col = new Document();
+		Document man = new Document();
+		Document acc = new Document();
+		String id_veh = (String)foundDocument.get((foundDocument.size()-1)).get("vehicle_id");
+		int idv = Integer.parseInt(id_veh) + 1;
+		documents.put("vehicle_id", Integer.toString(idv));
+		documents.put("license_plate_number", plate);
+		//color
+		String mId = Integer.toString(cols.getColorById(color).getColor_ID());
+		col.put("color_id", mId); 
+		col.put("description", cols.getColorById(color).getDescription());
+		col.put("manufacturer_color_code", cols.getColorById(color).getManufacturer_color_code());
+		documents.put("color", col);
+	
+		//model
+		String moId = Integer.toString(mods.getModelById(model).getModel_ID());
+		mod.put("model_id", moId);
+		//manufacturer
+		String id_str1 =Integer.toString(mans.getManufacturerById(manufacturer).getManufacturer_ID());
+		man.put("manufacturer_id", id_str1); 
+		man.put("name",mans.getManufacturerById(manufacturer).getName());
+		man.put("country",mans.getManufacturerById(manufacturer).getCountry());
+		mod.put("manufacturer", man);
+		String mop =Double.toString(mods.getModelById(model).getPrice());
+		mod.put("price",mop);
+		documents.put("model", mod);
+		//accessory
+		String ida =Integer.toString(acce.getAccessoryById(accessory).getAccessory_ID());
+		acc.put("accessory_id", ida);
+		acc.put("name",acce.getAccessoryById(accessory).getName());
+		acc.put("name",acce.getAccessoryById(accessory).getDescription());
+		documents.put("accessory", acc);
+		String mil = Integer.toString(mileage);
+		documents.put("mileage", mil);
+		String myear = Integer.toString(year);
+		documents.put("manufacturer_year", myear);
+		documents.put("type", "car");
+		String bool = Boolean.toString(active);
+		documents.put("active", bool);
+		/*
+		 * String len = (String) cols.get("length");
+						int length = Integer.parseInt(len);
+						String he = (String) cols.get("height");
+						int height = Integer.parseInt(he);
+						String loading = (String) cols.get("loading_limit");
+						int loading_limit = Integer.parseInt(loading);
+		 */
+		String len = Integer.toString(length);
+		documents.put("length", len);
+		String hei = Integer.toString(height);
+		documents.put("height", hei);
+		String loa = Integer.toString(load_limit);
+		documents.put("loading_limit", loa);
+		collection.insertOne(documents);			
+
+	}catch (MongoException e){
+	    System.out.println(e.getClass().getCanonicalName());
+	}finally {
+		client.close();
+	}
 	}
 	
 	public void changeCar(int vehicle_ID,String plate, int color, int model, int manufacturer, int accessory, int mileage, int year, Boolean active, int doors, int pass_limit){
